a44ef8c1341c98b344a8d7612a47aa6485c74c6a
armelcarelb-coder <armelcarelb@gmail.com>
2026-03-26 16:37:37 +0000

    creation de la premiere classe de mon model

---
+++ models/football_model.py
@@ -0,0 +1,16 @@
+class Footballanalystprobabilistic : 
+    def __int__(self, player_name):
+        self.player_name = player_name
+        self.model = None
+        self.trace = None
+    
+    def build_model(self) :
+        pass
+
+    def train(self) :
+        pass
+
+    def predict(self) :
+        pass
+
+    
